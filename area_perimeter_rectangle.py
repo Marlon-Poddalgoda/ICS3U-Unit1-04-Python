dd0318cd27e5ee408f5d55e758827732229d0be5
Ubuntu <ubuntu@ip-172-31-11-30.ec2.internal>
2020-11-20 17:50:29 +0000

    added area_perimeter_rectangle.py

---
+++ area_perimeter_rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by Marlon Poddalgoda
+# Created on November 19 2020
+# This is the "Hello, World!" program with proper style
+
+
+def main():
+    print("If a rectangle has dimensions:")
+    print("5cm x 3cm")
+    print("Then...")
+    print("Area is {}cm²".format(5*3))
+    print("Perimeter is {}cm".format(2*(5+3)))
+
+
+if __name__ == "__main__":
+    main()
